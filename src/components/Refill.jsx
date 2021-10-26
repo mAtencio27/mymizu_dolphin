db6e22916ed54b600ca3c2c1064eac187b1dde02
karin0216 <cheer.karin0216@gmail.com>
2021-10-26 16:20:21 +0900

    add refill button

--- src/components/Refill.jsx
+++ src/components/Refill.jsx
@@ -1,8 +1,27 @@
-
+import Button from 'react-bootstrap/Button'
 function Refill() {
   return (
     <div>
-      <button>Refill</button>
+     <>
+  <style type="text/css">
+    {`
+    .btn-flat {
+      background-color: #56DCEB;
+      color: white;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+      border-radius: 10px
+    }
+    `}
+  </style>
+
+  <Button variant="flat" size="xxl">
+    Refill
+  </Button>
+</>
     </div>
   );
 }
