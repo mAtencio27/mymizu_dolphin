52049a01d241a1dd455f114fff9f715c9b7da8e1
Cal <marsc0388@gmail.com>
2021-10-26 17:47:46 +0900

    Feat: Initial commit of endpoints
    Co-authored-by: Marcus Atencio <mAtencio27@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,6 +1,38 @@
 const express = require('express');
+const morgan = require('morgan');
+const axios = require('axios');
+const cors = require('cors');
+require("dotenv").config();
+
+const token = process.env.API_KEY
+
+const config = {
+    headers: {
+        'Authorization': `Bearer ${token}`
+    }
+}
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors({
+    origin: '*'
+}));
+
+app.get("/api/me", async (req, res, next) => {
+    console.log("/test call");
+    try {
+        console.log(config);
+        let rawdata = await axios.get("https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/me", config)
+        rawdata = JSON.stringify(rawdata);
+        res.send(rawdata);
+    }
+    catch (err) {
+        res.sendStatus(404);
+        console.error("Failed to connect to mymizu api", err)
+    }
+})
+
+//Endpoints go here
 
 module.exports = app;
