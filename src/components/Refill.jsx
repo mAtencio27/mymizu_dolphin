ff8bc4819198fa3b20f66908f1ae45d8bec1bcd5
karin0216 <cheer.karin0216@gmail.com>
2021-10-26 17:48:22 +0900

    create basic styles

--- src/components/Refill.jsx
+++ src/components/Refill.jsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button'
+import Milestone from './Milestone';
+
 function Refill() {
+  const [view, setView] = useState("Home");
+
+  const changeView = () => {
+    setView("Milestone");
+    console.log(view);
+  }
+
   return (
     <div>
       <>
         <style type="text/css">
           {`
-    .btn-flat {
+    .btn-refill {
       background-color: #56DCEB;
       color: white;
       font-weight: bolder;
       cursor: pointer;
-      margin: 10px;
+      margin: 15px;
     }
 
     .btn-xxl {
@@ -21,9 +31,35 @@ function Refill() {
     `}
         </style>
 
-        <Button variant="flat" size="xxl" className="log-refill">
+        <Button variant="refill" size="xxl" className="log-refill">
           Log refill
         </Button>
+      </>
+      <>
+        <style type="text/css">
+          {`
+    .btn-milestone {
+      background-color: #149FD4;
+      color: white;
+      font-weight: bolder;
+      cursor: pointer;
+      margin: 15px;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+      border-radius: 10px
+    }
+    `}
+        </style>
+
+        <Button variant="milestone" size="xxl" className="log-refill"
+          onClick={() => changeView()}
+        >
+          Check Milestone
+        </Button>
+        { view === "Milestone" ? <Milestone /> : null}
       </>
     </div>
   );
