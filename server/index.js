95d056b748aa448ca67d17478fdce797f638867b
mAtencio27 <mAtencio27@hotmail.com>
2021-10-27 10:59:02 +0900

    server fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require('./server');
 const db = require("./knex");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 (async () => {
     try {
