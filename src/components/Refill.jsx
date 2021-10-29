cc75ffda90210697f758e2aa3ccd419cff227330
Cal <marsc0388@gmail.com>
2021-10-29 10:24:43 +0900

    Feat: Add cup animation

--- src/components/Refill.jsx
+++ src/components/Refill.jsx
@@ -1,29 +1,41 @@
 import Button from 'react-bootstrap/Button'
+import cupStyles from '../styles/modules/cup.module.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Refill({user, handleUserChange, handleStoneChange}) {
 
   return (
     <div>
-      <h1>{user.refill_amount}L of water refilled</h1>
-        <style type="text/css">
-          {`
-    .btn-refill {
-      background-color: #56DCEB;
-      color: white;
-      font-weight: bolder;
-      cursor: pointer;
-      margin-top: 15px;
-      margin-left: 15px;
-      margin-right: 15px;
-    }
+    <style type="text/css">
+    {`
+.btn-refill {
+background-color: #56DCEB;
+color: white;
+font-weight: bolder;
+cursor: pointer;
+margin-top: 15px;
+margin-left: 15px;
+margin-right: 15px;
+}
 
-    .btn-xxl {
-      padding: 1rem 1.5rem;
-      font-size: 1.5rem;
-      border-radius: 10px
-    }
-    `}
-        </style>
+.btn-xxl {
+padding: 1rem 1.5rem;
+font-size: 1.5rem;
+border-radius: 10px
+}
+`}
+  </style>
+    <Container fluid="md">
+    <Row className="mt-4 mb-2 mx-auto">
+      <h1 className="text-center">{user.refill_amount}L of water refilled</h1>
+    </Row>
+    <Row className="mt-2 mb-4">
+      <div className={cupStyles.cup}></div>
+    </Row>
+    </Container>
+        
         <div className="d-grid gap-2">
           <Button
             variant="refill"
